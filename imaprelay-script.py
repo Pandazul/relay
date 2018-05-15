c3eef4b870e61f4751b429c5958ca1b056f5ccc7
Saul Barros <pandazulweb@gmail.com>
2018-05-15 21:28:19 +0100

    Code Updates

--- imaprelay-script.py
+++ imaprelay-script.py
@@ -1,6 +1,6 @@
 #!c:\python27\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'imaprelay==0.0.5','console_scripts','imaprelay'
-__requires__ = 'imaprelay==1.0'
+__requires__ = 'imaprelay==0.0.5'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('imaprelay==1.0', 'console_scripts', 'imaprelay')()
+        load_entry_point('imaprelay==0.0.5', 'console_scripts', 'imaprelay')()
     )
